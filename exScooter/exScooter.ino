056211f592a5c4499e5ccea26ed37fddee677224
Maciek-exPC <Maciek@DESKTOP-exPC>
2018-03-10 09:37:29 +0100

    Add project files.

---
+++ exScooter/exScooter.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		exScooter.ino
+ Created:	3/10/2018 9:37:20 AM
+ Author:	Maciek
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
