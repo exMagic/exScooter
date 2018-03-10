ddce28989e12950893c4eb92e2bebbbd32be5ede
Maciek-exPC <Maciek@DESKTOP-exPC>
2018-03-10 12:32:15 +0100

    add gyro project

---
+++ gyro/gyro.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		gyro.ino
+ Created:	3/10/2018 12:31:24 PM
+ Author:	Maciek
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
