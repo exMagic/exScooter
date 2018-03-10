b131892006089d2fddc9488261696b6bebd4842a
Maciek-exPC <Maciek@DESKTOP-exPC>
2018-03-10 09:41:13 +0100

    Big Bang

--- exScooter/exScooter.ino
+++ exScooter/exScooter.ino
@@ -1,12 +1,12 @@
 /*
  Name:		exScooter.ino
- Created:	3/10/2018 9:37:20 AM
+ Created:	10/03/2018 9:37:20 AM
  Author:	Maciek
 */
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	//
 }
 
 // the loop function runs over and over again until power down or reset
